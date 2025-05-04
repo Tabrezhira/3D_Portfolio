39f27bd97cf16498f541d5df22ff7aa7cb40a326
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-05-05 02:36:41 +0530

    Navbar Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,13 +29,13 @@ function Navbar() {
         <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => setToggle(!toggle)} />
           <div className={`${!toggle ? 'hidden' : "flex"} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
-            <ul className='list-none flex justify-end items-start flex-col gap-4'>
-            {navLinks.map((link) => (
-              <li onClick={() => {setToggle(!toggle); setActive(link.title)}} key={link.id} className={`${active === link.title ? 'text-white' : 'text-secondary'} font-poppins font-medium cursor-pointer`}>
-                <a href={`#${link.id}`}> {link.title}</a>
-              </li>
-            ))}
-            </ul>
+              <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li onClick={() => {setToggle(!toggle); setActive(link.title)}} key={link.id} className={`${active === link.title ? 'text-white' : 'text-secondary'} font-poppins font-medium cursor-pointer`}>
+                  <a href={`#${link.id}`}> {link.title}</a>
+                </li>
+              ))}
+              </ul>
           </div>
         </div>
       </div>
