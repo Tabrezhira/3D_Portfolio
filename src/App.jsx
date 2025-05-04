9bda1f951d7ecfe1e65f29f073487295511f3710
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-05-05 02:36:20 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,21 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Hello</h1>
+      <div className=" relative z-0 bg-primary">
+        <div className=" bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className=" relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
